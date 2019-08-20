9993ac56cfe0e3af06a21665fa23ad4c6bb8a93b
NiciMorger <54328006+NiciMorger@users.noreply.github.com>
2019-08-20 21:09:09 +0200

    Add files via upload

---
+++ helloworldbynici.sh
@@ -0,0 +1,14 @@
+#Aufgabe KPMG Schnuppertag
+#version 1.0 
+
+echo "H"
+echo -e "\e[31m E\e[0m"
+echo "  L"
+echo -e "\e[31m   L\e[0m"
+echo "    O"
+echo ""
+echo -e "\e[33m      W\e[0m"
+echo "       O"
+echo -e "\e[33m        R\e[0m"
+echo "         L"
+echo -e "\e[33m          D\e[0m"
